190f277adfa2b4c78f434fc6e442fa88c9302979
CrnoSrce <corey.spink@gmail.com>
2021-10-13 16:08:44 +1100

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
   
-  bucket_prefix = "corey-spink"
+  bucket_prefix = var.prefix
   acl    = "private"
 
   versioning = {
